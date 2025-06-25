c4adcefa298daa37fe2d7b10e5bf793a6e27797f
t-ski <t.schiepanski@gmx.de>
2025-06-25 10:01:36 +0200

    Implement evaluation summarization utility script

---
+++ eval/summarize.js
@@ -0,0 +1,39 @@
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+
+const EVALS = [
+    // TODO
+];
+
+
+const summary = {};
+
+await Promise.all(
+    EVALS.map(async resultsFileName => {
+        const resultsFilePath = join(import.meta.dirname, resultsFileName.replace(/(\.json)?$/, ".json"));
+        const results = (await import(resultsFilePath, { with: { type: "json" } })).default;
+
+        summary[resultsFileName] = {
+            success: 0,
+            failure: 0,
+            successRate: 0.0,
+            meanSnapshotSize: 0.0,
+            meanEstimatedTokens: 0.0
+        };
+        results.forEach((result, i) => {
+            const mean = (key1, key2) => {
+                return ((summary[resultsFileName][key1] ?? 0) + +result[key2]) / (i + 1);
+            };
+
+            summary[resultsFileName].success += +result.success;
+            summary[resultsFileName].failure += +!result.success;
+            summary[resultsFileName].successRate = mean("successRate", "success");
+            summary[resultsFileName].meanSnapshotSize = mean("meanSnapshotSize", "snapshotSize");
+            summary[resultsFileName].meanEstimatedTokens = mean("meanEstimatedTokens", "estimatedTokens");
+        });
+    })
+);
+
+
+writeFileSync(join(import.meta.dirname, "summary.json"), JSON.stringify(summary, null, 2));
