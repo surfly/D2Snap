fde57abf71e59b0c359f599af74c176073c58bc6
t-ski <t.schiepanski@gmx.de>
2025-06-25 09:54:25 +0200

    Add test cases

--- test/test.js
+++ test/test.js
@@ -51,3 +51,119 @@ async function test(title, cb) {
 
     await cb();
 }
+
+
+await test("Take adaptive DOM snapshot (4096)", async () => {
+    const snapshot = await takeAdaptiveSnapshot(readFile("agents"), 4096, 5, {
+        debug: true,
+        assignUniqueIDs: true
+    });
+
+    writeActual("agents.4096", snapshot.serializedHtml);
+
+    assertLess(
+        snapshot.serializedHtml.length / 4,
+        4096,
+        "Invalid adaptive DOM snapshot size"
+    );
+
+    assertLess(
+        2048,
+        snapshot.serializedHtml.length,
+        "Invalid adaptive DOM snapshot size"
+    );
+
+    assertIn(
+        flattenDOMSnapshot(`<a data-uid="7">About</a>`),
+        flattenDOMSnapshot(snapshot.serializedHtml),
+        "Interactive element not preserved"
+    );
+});
+
+await test("Take adaptive DOM snapshot (2048)", async() => {
+    const snapshot = await takeAdaptiveSnapshot(readFile("agents"), 2048, 5, {
+        debug: true,
+        assignUniqueIDs: true
+    });
+
+    writeActual("agents.2048", snapshot.serializedHtml);
+
+    assertLess(
+        snapshot.serializedHtml.length / 4,
+        2048,
+        "Invalid adaptive DOM snapshot size"
+    );
+});
+
+await test("Take DOM snapshot (L)", async () => {
+    const snapshot = await takeSnapshot(readFile("pizza"), 2, 6, 0.375, {
+        debug: true
+    });
+
+    writeActual("pizza.l", snapshot.serializedHtml);
+    const expected = readExpected("pizza.l");
+
+    assertAlmostEqual(
+        snapshot.meta.originalSize,
+        640,
+        -1,
+        "Invalid DOM snapshot original size"
+    );
+
+    assertAlmostEqual(
+        snapshot.meta.sizeRatio,
+        0.43,
+        2,
+        "Invalid DOM snapshot size ratio"
+    );
+
+    assertEqual(
+        flattenDOMSnapshot(snapshot.serializedHtml),
+        flattenDOMSnapshot(expected),
+        "Invalid DOM snapshot"
+    );
+});
+
+await test("Take DOM snapshot (M)", async() => {
+    const snapshot = await takeSnapshot(readFile("pizza"), 4, 4, 0.7, {
+        debug: true
+    });
+
+    writeActual("pizza.m", snapshot.serializedHtml);
+    const expected = readExpected("pizza.m");
+
+    assertAlmostEqual(
+        snapshot.meta.sizeRatio,
+        0.33,
+        2,
+        "Invalid DOM snapshot size ratio"
+    );
+
+    assertEqual(
+        flattenDOMSnapshot(snapshot.serializedHtml),
+        flattenDOMSnapshot(expected),
+        "Invalid DOM snapshot"
+    );
+});
+
+await test("Take DOM snapshot (S)", async () => {
+    const snapshot = await takeSnapshot(readFile("pizza"), 100, 2, 1.0, {
+        debug: true
+    });
+
+    writeActual("pizza.s", snapshot.serializedHtml);
+    const expected = readExpected("pizza.s");
+
+    assertAlmostEqual(
+        snapshot.meta.sizeRatio,
+        0.26,
+        2,
+        "Invalid DOM snapshot size ratio"
+    );
+
+    assertEqual(
+        flattenDOMSnapshot(snapshot.serializedHtml),
+        flattenDOMSnapshot(expected),
+        "Invalid DOM snapshot"
+    );
+});
