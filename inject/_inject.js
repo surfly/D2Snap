4eb92cc19cbde316c2342e8d8b5119c2f2c93a27
t-ski <t.schiepanski@gmx.de>
2025-06-25 09:55:45 +0200

    Add browser debug files with injected D2Snap execution

---
+++ inject/_inject.js
@@ -0,0 +1,30 @@
+document
+    .addEventListener("DOMContentLoaded", async() => {
+        const presentSnapshot = (snapshot, parameters = null) => {
+            console.log("-".repeat(25));
+            console.log(snapshot.parameters ?? parameters);
+            console.log(snapshot.meta);
+            console.log(snapshot.serializedHtml);
+        };
+
+        console.log(document.body.innerHTML);
+
+        presentSnapshot(
+            await D2Snap.takeSnapshot(2, 5, 0.375, {
+                debug: true
+            }),
+            { k: 2, l: 5, m: 0.375 }
+        );
+        presentSnapshot(
+            await D2Snap.takeSnapshot(4, 2, 0.6, {
+                debug: true
+            }),
+            { k: 4, l: 2, m: 0.6 }
+        );
+        presentSnapshot(
+            await D2Snap.takeAdaptiveSnapshot(undefined, undefined, {
+                debug: true,
+                assignUniqueIDs: true
+            })
+        );
+    });
