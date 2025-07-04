c8a50839077026ffd938b1b69c8bca681c013ce3
t-ski <t.schiepanski@gmx.de>
2025-07-04 13:56:18 +0200

    Improve test error log

--- test/test.js
+++ test/test.js
@@ -2,20 +2,43 @@ import { join } from "path";
 import { deepEqual as assertEqual, ok } from "assert";
 
 
-global.assertEqual = assertEqual;
+function wrapAssertion(cb, actual = null, expected = null, relationHint = null) {
+    try {
+        cb();
+    } catch(err) {
+        if(err.code !== "ERR_ASSERTION") {
+            console.error(err);
+
+            process.exit(1);
+        }
+
+        console.error("\x1b[31mAssertion Error\x1b[0m");
+        console.log(`\x1b[2mEXPECTED${relationHint ? `(${relationHint})` : ""}:\x1b[0m`, expected ?? err.expected);
+        console.log("\x1b[2mACTUAL:\x1b[0m", actual ?? err.actual);
+
+        process.exit(2);
+    }
+}
+
+global.assertEqual = function(a, b, message) {
+    wrapAssertion(() => assertEqual(a, b, message));
+}
 
 global.assertLess = function(a, b, message) {
-    return ok(a < b, message);
+    wrapAssertion(() => ok(a < b, message), a, b, "<");
 }
 
 global.assertIn = function(a, b, message) {
-    return ok(b.includes(a), message);
+    wrapAssertion(() => ok(b.includes(a), message), a, b, "in");
 }
 
 global.assertAlmostEqual = function(a, b, precision, message) {
     const roundPrecision = a => Math.round(a * 10**precision) / 10**precision;
 
-    return assertEqual(roundPrecision(a), roundPrecision(b), message);
+    const roundA = roundPrecision(a);
+    const roundB = roundPrecision(b);
+
+    wrapAssertion(() => assertEqual(roundA, roundB, message), roundA, roundB, "~");
 }
 
 global.path = function(fileName) {
